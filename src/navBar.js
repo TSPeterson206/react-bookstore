7db7df48634a69b1624c29090d357ccc07ec5250
TOBY PETERSON <TSPETERSON@)^@GMAIL.COM>
2019-01-09 16:24:06 -0800

    here

--- src/navBar.js
+++ src/navBar.js
@@ -4,9 +4,8 @@ import './index.css';
 function NavBar () {
   return (
     <nav className="navbar navbar-dark bg-primary">
-    <div className="navText">
+    
   <p className="navbar-brand" href="#">Welcome to the React Bookstore!</p>
-  </div> 
 </nav>
   )
 }
